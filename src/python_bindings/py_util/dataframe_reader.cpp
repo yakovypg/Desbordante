a8afd984afb04c8d8fd52cc0412c306c88f8375d
yakovypg <yasha-pmp@mail.ru>
2023-08-06 19:21:50 +0300

    Change size_t to std::size_t

--- src/python_bindings/py_util/dataframe_reader.cpp
+++ src/python_bindings/py_util/dataframe_reader.cpp
@@ -42,7 +42,7 @@ std::string DataframeReaderBase::GetColumnName(size_t index) const {
     return column_names_.at(index);
 }
 
-size_t DataframeReaderBase::GetNumberOfColumns() const {
+std::size_t DataframeReaderBase::GetNumberOfColumns() const {
     return column_names_.size();
 }
 
