8fb1544b298dabd971429365a33e6a277d94e13d
yakovypg <yasha-pmp@mail.ru>
2023-08-29 16:39:28 +0300

    Add tests

--- src/algorithms/fastod/stripped_partition.cpp
+++ src/algorithms/fastod/stripped_partition.cpp
@@ -6,7 +6,7 @@
 using namespace algos::fastod;
 
 // important
-CacheWithLimit<size_t, StrippedPartition> StrippedPartition::cache_(1e4);
+CacheWithLimit<size_t, StrippedPartition> StrippedPartition::cache_(1e8);
 
 StrippedPartition::StrippedPartition(const DataFrame& data) : data_(std::move(data)) {
     indexes_.reserve(data.GetTupleCount());
