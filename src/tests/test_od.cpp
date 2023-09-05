e101b6179053694a623d5df527b152597ab68b40
Michael Polyntsov <Arno9148@gmail.com>
2023-09-05 21:05:07 +0300

    Move DataFrame to FastOD instead of copying it

--- src/tests/test_od.cpp
+++ src/tests/test_od.cpp
@@ -15,7 +15,7 @@ size_t RunFastOdOn(std::string dataset) {
     algos::fastod::DataFrame data =
         algos::fastod::DataFrame::FromCsv(d);
 
-    algos::fastod::Fastod fastod(data, std::numeric_limits<long>::max());
+    algos::fastod::Fastod fastod(std::move(data), std::numeric_limits<long>::max());
     std::vector<algos::fastod::CanonicalOD> ods = fastod.Discover();
     std::vector<std::string> string_ods;
     string_ods.reserve(ods.size());
