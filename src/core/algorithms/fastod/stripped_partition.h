eccfe5a7d5d833df4d9bb54f113c3f96033832b2
yakovypg <yasha-pmp@mail.ru>
2023-11-01 08:15:45 +0300

    Add range based stripped partition

--- src/core/algorithms/fastod/stripped_partition.h
+++ src/core/algorithms/fastod/stripped_partition.h
@@ -4,21 +4,26 @@
 #include <unordered_map>
 #include <algorithm>
 
+#include "range_based_stripped_partition.h"
 #include "single_attribute_predicate.h"
 #include "attribute_set.h"
 
 namespace algos::fastod {
+
+class RangeBasedStrippedPartition;
 
 class StrippedPartition {
 private:
     std::vector<size_t> indexes_;
     std::vector<size_t> begins_;
     const DataFrame& data_;
+
+    StrippedPartition(const DataFrame& data, std::vector<size_t> const& indexes, std::vector<size_t> const& begins);
+    friend class RangeBasedStrippedPartition;
     
 public:
     explicit StrippedPartition(const DataFrame& data);
     StrippedPartition(const StrippedPartition& origin) = default;
-    //StrippedPartition(const DataFrame& data, std::vector<size_t> const& indexes, std::vector<size_t> const& begins);
 
     std::string ToString() const;
     StrippedPartition& operator=(const StrippedPartition& other);
--- src/core/algorithms/fastod/stripped_partition_cache.h
+++ src/core/algorithms/fastod/stripped_partition_cache.h
@@ -1,24 +1,26 @@
 #pragma once
 
-#include "stripped_partition.h"
 #include "cache_with_limit.h"
-
-#define STRIPPED_PARTITION StrippedPartition
+#include "stripped_partition.h"
+#include "range_based_stripped_partition.h"
 
 namespace algos::fastod {
 
 class StrippedPartitionCache {
+public:
+    using partition_t = StrippedPartition;
+
 private:
-    CacheWithLimit<AttributeSet, STRIPPED_PARTITION> cache_{static_cast<size_t>(1e8)};
+    CacheWithLimit<AttributeSet, partition_t> cache_{static_cast<size_t>(1e8)};
 
 public:
-    STRIPPED_PARTITION GetStrippedPartition(AttributeSet const& attribute_set,
-                                           DataFrame const& data) {
+    StrippedPartitionCache::partition_t GetStrippedPartition(AttributeSet const& attribute_set,
+                                                             DataFrame const& data) {
         if (cache_.Contains(attribute_set)) {
             return cache_.Get(attribute_set);
         }
 
-        std::optional<STRIPPED_PARTITION> result;
+        std::optional<StrippedPartitionCache::partition_t> result;
 
         auto callProduct = [&result](size_t attr) {
             result->Product(attr);
@@ -35,7 +37,7 @@ public:
         }
 
         if (!result) {
-            result = STRIPPED_PARTITION(data);
+            result = StrippedPartitionCache::partition_t(data);
 
             for (AttributeSet::size_type attr = attribute_set.find_first(); attr != AttributeSet::npos;
                  attr = attribute_set.find_next(attr)) {
