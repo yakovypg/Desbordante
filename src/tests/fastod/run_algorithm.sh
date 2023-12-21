9db84a3865b7e1878b79f07b4f179c9242526cb3
yakovypg <yasha-pmp@mail.ru>
2023-12-21 15:30:34 +0300

    Fix getting datasets from arguments

--- src/tests/fastod/run_algorithm.sh
+++ src/tests/fastod/run_algorithm.sh
@@ -3,7 +3,7 @@
 datasets='../../../test_input_data/od_norm_data/metanome/*.csv'
 
 if [[ -n $1 ]]; then
-	datasets=$1
+	datasets=$@
 fi
 
 python3 run_algorithm.py -i ../../../build/target/Desbordante_run -d $datasets
--- src/tests/fastod/run_tests_with_cpp.sh
+++ src/tests/fastod/run_tests_with_cpp.sh
@@ -9,7 +9,7 @@ second_alg_path=$1
 datasets='../../../test_input_data/od_norm_data/metanome/*.csv'
 
 if [[ -n $2 ]]; then
-	datasets=$2
+	datasets="${@:2}"
 fi
 
 python3 test_with_cpp.py -f ../../../build/target/Desbordante_run -s $second_alg_path -c ./sorted_compare.sh -d $datasets
