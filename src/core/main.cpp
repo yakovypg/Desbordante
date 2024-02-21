1c493c6aec802d5fd2d04a1c9c56aab492208e8c
yakovypg <yasha-pmp@mail.ru>
2024-02-21 13:41:43 +0300

    Restore source main.cpp

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -8,77 +8,23 @@
 #include "config/all_options.h"
 #include "util/enum_to_available_values.h"
 
-#include "algorithms/fastod/fastod.h"
-#include <climits>
-#include <string>
-#include <filesystem>
-#include <vector>
-#include <fstream>
-
 INITIALIZE_EASYLOGGINGPP
 
-#define NO_OUTPUT "NULL"
+namespace {
+constexpr auto kHelp = "help";
+constexpr auto kAlgorithm = "algorithm";
+constexpr auto kDHelp = "print the help message and exit";
 
-int findAndPrintOD(algos::fastod::DataFrame&& data, size_t max_time, const std::string& output_path) {  
-    algos::fastod::Fastod fastod(std::move(data), max_time);
-    std::vector<std::string> ods = fastod.DiscoverAsStrings();
-
-    if (output_path.size() == 0) {
-        std::cout << "Found ODs: " << ods.size() << '\n';
-
-        for (size_t i = 0; i < ods.size(); ++i)
-            std::cout << ods[i] << '\n';
-    }
-    else if (output_path == NO_OUTPUT) {
-        return 0;
-    }
-    else {
-        std::ofstream file;
-        file.open(output_path);
-
-        if (!file.is_open()) {
-            std::cout << "Error: failed to open output file\n";
-            return 2;
-        }
-
-        for (size_t i = 0; i < ods.size(); ++i)
-            file << ods[i] << '\n';
-
-        file.close();
-    }
-    return 0;
+boost::program_options::options_description InfoOptions() {
+    namespace po = boost::program_options;
+    po::options_description info_options("Desbordante information options");
+    info_options.add_options()(kHelp, kDHelp)
+            // --version, if needed, goes here too
+            ;
+    return info_options;
 }
+}  // namespace
 
-int main(int argc, char const* argv[]) {
-    std::string path_to_dataset = std::string("");
-    std::string output_path = std::string("");  
-    int max_time = INT_MAX;
-
-    if (argc <= 1) {
-        std::cout << "Error: Dataset not specified\n";
-        return 1; 
-    }
-    if (argc > 4) {
-        std::cout << "Error: Too many arguments entered\n";
-        return 1;
-    }
-
-    if (argc >= 2)
-        path_to_dataset = std::string(argv[1]);
-    
-    if (argc >= 3)
-        output_path = std::string(argv[2]);
-
-    if (argc >= 4) {
-        max_time = std::stoi(argv[3]);
-    }
-    
-    algos::fastod::DataFrame data = algos::fastod::DataFrame::FromCsv(std::filesystem::path(path_to_dataset));
-
-    return findAndPrintOD(std::move(data), max_time, output_path);
-}
-
-/*
 int main(int argc, char const* argv[]) {
     namespace po = boost::program_options;
     using namespace config;
@@ -131,4 +77,3 @@ int main(int argc, char const* argv[]) {
 
     return 0;
 }
-*/
