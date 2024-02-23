04acc77c1234037c11626d0df7dfb65311a07acb
yakovypg <yasha-pmp@mail.ru>
2024-02-23 22:43:12 +0300

    Fix fastod tests

--- src/tests/test_od.cpp
+++ src/tests/test_od.cpp
@@ -10,54 +10,60 @@
 
 namespace {
 
+namespace fs = std::filesystem;
 constexpr auto input_data_path = "input_data/od_norm_data/";
 
-size_t RunFastOdOn(std::string dataset, std::string folder) {
-    auto d = input_data_path / std::filesystem::path(folder + "/" + dataset);
-    algos::fastod::DataFrame data = algos::fastod::DataFrame::FromCsv(d);
+size_t RunFastod(std::string const& dataset_name, std::string const& subfolder) {
+    fs::path dataset_path = fs::path(input_data_path) / subfolder / dataset_name;
 
+    algos::fastod::DataFrame data = algos::fastod::DataFrame::FromCsv(dataset_path);
     algos::fastod::Fastod fastod(std::move(data), std::numeric_limits<long>::max());
+
     std::vector<std::string> string_ods = fastod.DiscoverAsStrings();
     std::sort(std::begin(string_ods), std::end(string_ods));
-    return boost::hash<decltype(string_ods)>()(string_ods);
+
+    std::size_t result_hash = 0;
+
+    for (std::string const& od : string_ods) {
+        size_t od_hash = std::hash<std::string>{}(od);
+        boost::hash_combine(result_hash, od_hash);
+    }
+
+    return result_hash;
 }
 
 }  // namespace
 
-using DatasetWithHashAndFolder = std::tuple<std::string, size_t, std::string>;
+using DatasetPathAndHash = std::tuple<std::string, std::string, size_t>;
 
-class TestFastOD : public ::testing::TestWithParam<DatasetWithHashAndFolder> {};
+class TestFastod : public ::testing::TestWithParam<DatasetPathAndHash> {};
 
-TEST_P(TestFastOD, CorrectnessTest) {
-    auto const& [dataset, expected_hash, folder] = GetParam();
-    size_t actual_hash = RunFastOdOn(dataset, folder);
+TEST_P(TestFastod, CorrectnessTest) {
+    auto const& [subfolder, dataset, expected_hash] = GetParam();
+    size_t actual_hash = RunFastod(dataset, subfolder);
     EXPECT_EQ(actual_hash, expected_hash);
 }
 
 INSTANTIATE_TEST_SUITE_P(
-        TestFastODSuite, TestFastOD,
+        TestFastodSuite, TestFastod,
         ::testing::Values(
-                DatasetWithHashAndFolder{"CLASSIFICATION_norm.csv", 18091212580651051245ULL,
-                                         "metanome"},
-                DatasetWithHashAndFolder{"OD_norm.csv", 17715088954325427783ULL, "."},
-                DatasetWithHashAndFolder{"abalone_norm.csv", 16606050467380301578ULL, "metanome"},
-                DatasetWithHashAndFolder{"balance-scale_norm.csv", 0ULL, "metanome"},
-                DatasetWithHashAndFolder{"breast-cancer-wisconsin.csv", 16353518380015578090ULL,
-                                         "metanome"},
-                DatasetWithHashAndFolder{"echocardiogram_norm.csv", 326215408910846314ULL,
-                                         "metanome"},
-                DatasetWithHashAndFolder{"hepatitis_norm.csv", 7742226892016166457ULL, "metanome"},
-                DatasetWithHashAndFolder{"hepatitis_norm_1.csv", 17523839062965376704ULL,
-                                         "metanome"},
-                DatasetWithHashAndFolder{"hepatitis_norm_2.csv", 6299823600881027889ULL,
-                                         "metanome"},
-                DatasetWithHashAndFolder{"hepatitis_norm_3.csv", 14366259447601078560ULL,
-                                         "metanome"},
-                DatasetWithHashAndFolder{"hepatitis_norm_4.csv", 17523839062965376704ULL,
-                                         "metanome"},
-                DatasetWithHashAndFolder{"hepatitis_norm_5.csv", 10213031677476394156ULL,
-                                         "metanome"},
-                DatasetWithHashAndFolder{"horse_10c_norm.csv", 943929269970913831ULL, "metanome"},
-                DatasetWithHashAndFolder{"iris_norm.csv", 0, "metanome"},
-                DatasetWithHashAndFolder{"small_2x3.csv", 14955030808612352839ULL, "."},
-                DatasetWithHashAndFolder{"small_3x3.csv", 3159022758505113431ULL, "."}));
+                DatasetPathAndHash{"metanome", "abalone_norm.csv", 14591019243917482774ULL},
+                DatasetPathAndHash{"metanome", "balance-scale_norm.csv", 0ULL},
+                DatasetPathAndHash{"metanome", "breast-cancer-wisconsin.csv",
+                                   14755061899855898930ULL},
+                DatasetPathAndHash{"metanome", "CLASSIFICATION_norm.csv", 14893168815353134583ULL},
+                DatasetPathAndHash{"metanome", "echocardiogram_norm.csv", 158822294474149028ULL},
+                DatasetPathAndHash{"metanome", "hepatitis_norm_1.csv", 14388080417062769138ULL},
+                DatasetPathAndHash{"metanome", "hepatitis_norm_2.csv", 4325460878791588226ULL},
+                DatasetPathAndHash{"metanome", "hepatitis_norm_3.csv", 15687519739058379771ULL},
+                DatasetPathAndHash{"metanome", "hepatitis_norm_4.csv", 14388080417062769138ULL},
+                DatasetPathAndHash{"metanome", "hepatitis_norm_5.csv", 1324052777825770237ULL},
+                DatasetPathAndHash{"metanome", "hepatitis_norm.csv", 14839749394849763630ULL},
+                DatasetPathAndHash{"metanome", "horse_10c_norm.csv", 16633700195346153616ULL},
+                DatasetPathAndHash{"metanome", "iris_norm.csv", 0ULL},
+                DatasetPathAndHash{"rand", "rand_30000x30.csv", 16311507802607191318ULL},
+                DatasetPathAndHash{"rand", "rand_60000x30.csv", 14912818095902024302ULL},
+                DatasetPathAndHash{"rand", "rand_120000x30.csv", 17702107539916091697ULL},
+                DatasetPathAndHash{".", "OD_norm.csv", 17439211062045545527ULL},
+                DatasetPathAndHash{".", "small_2x3.csv", 12135977562528923031ULL},
+                DatasetPathAndHash{".", "small_3x3.csv", 4292031651922569007ULL}));
