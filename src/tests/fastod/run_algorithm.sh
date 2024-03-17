c647e4b1726610faa6afb274bb05d24f10622083
yakovypg <yasha-pmp@mail.ru>
2024-03-17 15:38:14 +0300

    Remove scripts for Fastod experiments

--- src/tests/fastod/run_algorithm.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/bash
-
-datasets='../../../test_input_data/od_norm_data/metanome/*.csv'
-
-if [[ -n $1 ]]; then
-	datasets=$@
-fi
-
-python3 run_algorithm.py -i ../../../build/target/Desbordante_run -d $datasets
