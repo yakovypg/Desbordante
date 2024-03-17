98623c61aa43060a0c88e2846ab62bc07437d421
yakovypg <yasha-pmp@mail.ru>
2024-03-17 15:23:54 +0300

    Avoid using std::optional in cache

--- src/core/algorithms/od/fastod/stripped_partition_cache.h
+++ src/core/algorithms/od/fastod/stripped_partition_cache.h
@@ -13,26 +13,17 @@ class StrippedPartitionCache {
 private:
     CacheWithLimit<AttributeSet, ComplexStrippedPartition> cache_{static_cast<size_t>(1e8)};
 
-public:
-    void Clear() {
-        cache_.Clear();
+    void CallProductWithAttribute(ComplexStrippedPartition& partition, size_t attribute) {
+        partition.Product(attribute);
+
+        if (partition.ShouldBeConvertedToStrippedPartition()) {
+            partition.ToStrippedPartition();
+        }
     }
 
-    ComplexStrippedPartition GetStrippedPartition(AttributeSet const& attribute_set,
-                                                  DataFrame const& data) {
-        if (cache_.Contains(attribute_set)) {
-            return cache_.Get(attribute_set);
-        }
-
-        std::optional<ComplexStrippedPartition> result;
-
-        auto call_product = [&result](size_t attr) {
-            result->Product(attr);
-
-            if (result->ShouldBeConvertedToStrippedPartition()) {
-                result->ToStrippedPartition();
-            }
-        };
+    bool CallProductWithAttributesInCache(ComplexStrippedPartition& result,
+                                          AttributeSet const& attribute_set) {
+        bool is_product_called = false;
 
         for (AttributeSet::size_type attr = attribute_set.find_first();
              attr != attribute_set.size(); attr = attribute_set.find_next(attr)) {
@@ -40,23 +31,40 @@ public:
 
             if (one_less.any() && cache_.Contains(one_less)) {
                 result = cache_.Get(one_less);
-                call_product(attr);
+                CallProductWithAttribute(result, attr);
+                is_product_called = true;
             }
         }
 
-        if (!result) {
-            result = data.IsAttributesMostlyRangeBased(attribute_set)
-                             ? ComplexStrippedPartition::Create<true>(data)
-                             : ComplexStrippedPartition::Create<false>(data);
+        return is_product_called;
+    }
 
+public:
+    void Clear() {
+        cache_.Clear();
+    }
+
+    ComplexStrippedPartition GetStrippedPartition(AttributeSet const& attribute_set,
+                                                  std::shared_ptr<DataFrame> data) {
+        if (cache_.Contains(attribute_set)) {
+            return cache_.Get(attribute_set);
+        }
+
+        ComplexStrippedPartition result_partition;
+        bool is_product_called = CallProductWithAttributesInCache(result_partition, attribute_set);
+
+        if (!is_product_called) {
+            result_partition = data->IsAttributesMostlyRangeBased(attribute_set)
+                                       ? ComplexStrippedPartition::Create<true>(data)
+                                       : ComplexStrippedPartition::Create<false>(data);
             for (AttributeSet::size_type attr = attribute_set.find_first();
                  attr != attribute_set.size(); attr = attribute_set.find_next(attr)) {
-                call_product(attr);
+                CallProductWithAttribute(result_partition, attr);
             }
         }
 
-        cache_.Set(attribute_set, *result);
-        return std::move(result.value());
+        cache_.Set(attribute_set, result_partition);
+        return result_partition;
     }
 };
 
