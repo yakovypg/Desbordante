ac5bf5c805b73281db6ba00eadaffad2fb18a257
yakovypg <yasha-pmp@mail.ru>
2024-03-17 15:32:46 +0300

    Remove inline

--- src/tests/test_fastod.cpp
+++ src/tests/test_fastod.cpp
@@ -18,7 +18,7 @@
 
 namespace tests {
 
-inline size_t CombineHashes(size_t first, size_t second) {
+size_t CombineHashes(size_t first, size_t second) {
     size_t wave = second + 2654435769UL + (first << 6) + (first >> 2);
     return first ^ wave;
 }
