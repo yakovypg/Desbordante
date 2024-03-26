a6de4f61f6d11d5e3054b86c4dd17a1e5e39a273
yakovypg <yasha-pmp@mail.ru>
2024-03-27 00:17:24 +0300

    Add information about supporting of discovery of exact canonical order dependencies

--- cli/cli.py
+++ cli/cli.py
@@ -100,9 +100,10 @@ Currently, the console version of Desbordante supports:
 1) Discovery of exact functional dependencies
 2) Discovery of approximate functional dependencies
 3) Discovery of probabilistic functional dependencies
-4) Verification of exact functional dependencies
-5) Verification of approximate functional dependencies
-6) Verification of metric dependencies
+4) Discovery of exact canonical order dependencies
+5) Verification of exact functional dependencies
+6) Verification of approximate functional dependencies
+7) Verification of metric dependencies
 
 If you need other types, you should look into the C++ code, the Python
 bindings or the Web version.
